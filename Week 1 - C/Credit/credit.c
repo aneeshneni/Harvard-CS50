52b1cb772f0c09851b6729a1a1bb255fdcf61cd3
aneeshneni <44904997+aneeshneni@users.noreply.github.com>
2020-07-02 13:46:40 -0400

    Create credit.c

---
+++ Week 1 - C/Credit/credit.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+    long number = get_long("Enter Credit Card Number: ");
+    int tester = number;
+    int length = 0;
+    bool boo = false;
+    while (tester != 0) {
+        length++;
+        tester /= 10;
+    }
+    int eq = (number%1000000000000000);
+    if (length == 15) {
+        printf("American Express\n");
+        boo = true;
+    }
+    else if (length == 13) {
+        printf("Visa\n");
+        boo = true;
+    }
+    else if (length == 16 && eq == 4) {
+        printf("Visa\n");
+        boo = true;
+    }
+    else if (length == 16 && ((eq == 51) || (eq == 53) || (eq == 53) || (eq == 54) || (eq == 55))) {
+        printf("MasterCard\n");
+        boo = true;
+    }
+    else {
+        printf("Invalid Card");
+        boo = false;
+    }
+}
